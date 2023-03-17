659ce0a280a514264b5e29189c3656edc30a294d
Hector Selley <hector.selley@gmail.com>
2023-03-17 16:41:03 -0600

    Agregué la familia de tipografía libertinus

--- 1.repos.sh
+++ 1.repos.sh
@@ -5,13 +5,20 @@ sudo zypper dup
 
 # Paquetes básicos
 sudo zypper in -y exa fish zsh git-subrepo-zsh-completion flatpak-zsh-completion exa-zsh-completion alacritty-zsh-completion neovim alacritty alacritty alacritty-fish-completion alacritty-bash-completion alacritty-zsh-completion
-chsh -s /usr/bin/fish
+#chsh -s /usr/bin/fish
 
 # Packman
 sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ "Packman Repository"
 sudo zypper dup --from "Packman Repository" --allow-vendor-change
+
+# Familias de fuentes tipográficas
 sudo zypper in -y fetchmsttfonts free-ttf-fonts terminus-ttf-fonts opengost-ttf-fonts saja-cascadia-code-fonts google-inconsolata-fonts fira-code-fonts lato-fonts hack-fonts ubuntu-fonts fontawesome-fonts fontawesome-fonts-web
+sudo zypper in libertinus-fonts libertinus-fonts texlive-libertinus-doc texlive-libertinus-fonts texlive-libertinus-fonts-doc texlive-libertinus-fonts-fonts texlive-libertinus-otf-doc texlive-libertinus-type1 texlive-libertinus-type1-doc texlive-libertinus-type1-fonts texlive-libertinust1math texlive-libertinust1math-doc texlive-libertinust1math-fonts  
+
+# Aplicaciones varias
 sudo zypper in -y chromium chromium-ffmpeg-extra chromium-plugin-widevinecdm opera telegram-desktop tilix nautilus-extension-tilix tilix-lang opi
+
+# Multimedia
 sudo zypper in -y mpd mpd-doc mpDris2 mpDris2-lang ncmpcpp clementine tuner spotify-easyrpm vlc vlc-codecs
 spotify-easyrpm
 
