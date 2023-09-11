475d489a8ee8957b00d92cb6718ea72431b28dc5
Hector Selley <hector.selley@gmail.com>
2023-09-10 18:25:28 -0600

    Agregué btop

--- 4.extra.sh
+++ 4.extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Extra software 
-sudo zypper in -y nestopia screenfetch neofetch htop rofi rofi-calc dmenu bitwarden
+sudo zypper in -y nestopia screenfetch neofetch htop btop rofi rofi-calc dmenu bitwarden
 sudo zypper in -y touchegg
 sudo systemctl enable touchegg
