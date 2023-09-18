2cf2dfb6a3bb43e0a26af26495a7da79b28cd1c6
Hector Selley <hector.selley@gmail.com>
2023-09-17 23:28:14 -0600

    Agregué/quité paquetes
    Agregué:
    * nvtop
    * fastfetch
    
    Quité:
    * exa-zsh-completions

--- 1.repos.sh
+++ 1.repos.sh
@@ -4,8 +4,8 @@
 sudo zypper dup
 
 # Paquetes básicos
-sudo zypper in -y exa fish zsh git-subrepo-zsh-completion flatpak-zsh-completion exa-zsh-completion alacritty-zsh-completion neovim alacritty alacritty alacritty-fish-completion alacritty-bash-completion alacritty-zsh-completion
-#chsh -s /usr/bin/fish
+sudo zypper in -y exa fish zsh git-subrepo-zsh-completion flatpak-zsh-completion alacritty-zsh-completion neovim alacritty alacritty alacritty-fish-completion alacritty-bash-completion alacritty-zsh-completion
+chsh -s /usr/bin/fish
 
 # Packman
 sudo zypper ar -cfp 90 https://ftp.gwdg.de/pub/linux/misc/packman/suse/openSUSE_Tumbleweed/ "Packman Repository"
--- 4.extra.sh
+++ 4.extra.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Extra software 
-sudo zypper in -y nestopia screenfetch neofetch htop btop rofi rofi-calc dmenu bitwarden speedtest-cli
+sudo zypper in -y nestopia screenfetch neofetch fastfetch htop btop nvtop rofi rofi-calc dmenu bitwarden speedtest-cli
 sudo zypper in -y touchegg
 sudo systemctl enable touchegg
